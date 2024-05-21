9df0f2c4365634feb93fbb72eb5c3790cff44c77
Laura Richer <lauraricher@ip-10-0-0-57.eu-west-2.compute.internal>
2024-05-21 17:58:31 +0100

    linting & pre commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        alias: {
+          '~assets': './src/assets',
+          '~atoms': './src/components/atoms',
+          '~molecules': './src/components/molecules',
+          '~screens': './src/screens',
+          '~store': './src/store',
+          '~utils': './src/utils',
+        },
+        extensions: ['.js', '.jsx', '.tsx', '.ios.js', '.android.js'],
+      },
+    ],
+  ],
 };
