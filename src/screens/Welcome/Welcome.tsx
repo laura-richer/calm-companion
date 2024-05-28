12ad1f3af9ec295526e19a3ecd703b5fa1190915
laura-richer <lauraricher@gmail.com>
2024-05-28 09:01:49 +0100

    navigation

---
+++ src/screens/Welcome/Welcome.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export const Welcome = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
